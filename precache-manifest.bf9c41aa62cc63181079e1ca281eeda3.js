d60880e313bd32e265152218f42a001a547a31c8
James Finkelstein <jamesfinkelstein@Jamess-MacBook.local>
2019-01-24 13:59:44 -0500

    Updates

---
+++ precache-manifest.bf9c41aa62cc63181079e1ca281eeda3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ff2ccddacb8a6e70e7b",
+    "url": "/songList/static/js/runtime~main.5ff2ccdd.js"
+  },
+  {
+    "revision": "6cb5d54b31babc31835c",
+    "url": "/songList/static/js/main.6cb5d54b.chunk.js"
+  },
+  {
+    "revision": "8d8cbbf2639cf1685e67",
+    "url": "/songList/static/js/1.8d8cbbf2.chunk.js"
+  },
+  {
+    "revision": "6cb5d54b31babc31835c",
+    "url": "/songList/static/css/main.b0eccf5c.chunk.css"
+  },
+  {
+    "revision": "25616ca9ef6d5539d18306d802689df9",
+    "url": "/songList/index.html"
+  }
+];
